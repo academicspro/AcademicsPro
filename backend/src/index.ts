f8ea30aba6ddb0767ee083fd44de3cd7947834bd
rajneeshrana0 <rajneeshrana038@gmail.com>
2025-02-06 09:15:41 +0530

    Update build script to include Prisma generation and clean up code formatting

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,15 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
-import  schoolRoute  from "./routes/superadmin/schoolRoute";
+import schoolRoute from "./routes/superadmin/schoolRoute";
 import superAdminRoute from "./routes/superadmin/superAdminRoute";
 import signinRoute from "./routes/signin/signinRoute";
 import forgotRoute from "./routes/forgot-password/forgotRoute";
 
 const app = express();
 
-app.use(cors()); 
-app.use(express.json()); 
-
+app.use(cors());
+app.use(express.json());
 
 app.use(
   morgan(
@@ -19,22 +18,18 @@ app.use(
   )
 );
 
-
 app.get("/api/v1/health", (req, res) => {
   res.send("Backend is live");
 });
 
-
-
-
 // Routes Started
 
-app.use("/api/v1",schoolRoute)
+app.use("/api/v1", schoolRoute);
 
 // Auth Routes
 app.use("/api/v1", superAdminRoute);
-app.use('/api/v1',signinRoute);
-app.use('/api/v1', forgotRoute);
+app.use("/api/v1", signinRoute);
+app.use("/api/v1", forgotRoute);
 
 const PORT = process.env.PORT || 3000;
 
