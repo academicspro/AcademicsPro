88665a213d17355c580686fe1fb964579e690eb4
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2025-02-08 22:58:42 +0530

    few changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import chalk from "chalk";
-import morgan from "morgan"; 
-import { Response,Request } from "express";
+// import chalk from "chalk";
+// import morgan from "morgan"; 
+// import { Response,Request } from "express";
 
 import schoolRoute from "./routes/superadmin/schoolRoute";
 import superAdminRoute from "./routes/superadmin/superAdminRoute";
@@ -14,15 +14,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(
-  morgan((tokens, req: Request, res: Response) => {
-    return chalk.greenBright.bold(`[💻] ${tokens.method(req, res)}`) +
-      chalk.cyan(` ${tokens.url(req, res)}`) +
-      chalk.yellowBright(` ${tokens.status(req, res)}`) +
-      chalk.magentaBright(` ${tokens['response-time'](req, res)}ms`) +
-      chalk.red(` [IP: ${req.ip}]`);
-  })
-);
+// app.use(
+//   morgan((tokens, req: Request, res: Response) => {
+//     return chalk.greenBright.bold(`[💻] ${tokens.method(req, res)}`) +
+//       chalk.cyan(` ${tokens.url(req, res)}`) +
+//       chalk.yellowBright(` ${tokens.status(req, res)}`) +
+//       chalk.magentaBright(` ${tokens['response-time'](req, res)}ms`) +
+//       chalk.red(` [IP: ${req.ip}]`);
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("Backend is live");
