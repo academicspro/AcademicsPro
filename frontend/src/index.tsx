64dcf53008ca437b99cbe4c6b1c75e81931c275f
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2025-02-11 22:58:04 +0530

    Seprate Frontend folder Created

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { base_path } from "./environment";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../src/style/css/feather.css";
-import "../src/index.scss";
+import "./index.scss";
 import store from "./core/data/redux/store";
 import { Provider } from "react-redux";
 import "../src/style/icon/boxicons/boxicons/css/boxicons.min.css";
