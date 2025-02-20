430c21553c1dbcb0d69bddc6a317944939472800
rajneeshrana0 <rajneeshrana038@gmail.com>
2025-02-20 14:48:15 +0530

    multiplefile upload support

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -16,7 +16,12 @@ cloudinary.config({
  * @param folder - The folder where the file should be stored in Cloudinary.
  * @returns An object containing the public ID and URL of the uploaded image.
  */
-export async function uploadFile(buffer: Buffer, folder: string): Promise<{ publicId: string; url: string }> {
+export async function uploadFile(
+  buffer: Buffer, 
+  folder: string,
+  
+
+): Promise<{ publicId: string; url: string }> {
   return new Promise<{ publicId: string; url: string }>((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       { folder },
@@ -35,41 +40,3 @@ export async function uploadFile(buffer: Buffer, folder: string): Promise<{ publ
 }
 
 
-
-
-// Faster upload For later and image quality optimization
-
-
-
-// import cloudinary from 'cloudinary';
-
-// /**
-//  * Uploads multiple images to Cloudinary with size optimization.
-//  * @param buffers - An array of image files as Buffers.
-//  * @param folder - The folder where the files should be stored in Cloudinary.
-//  * @returns An array of objects containing the public ID and URL of each uploaded image.
-//  */
-// export async function uploadMultipleFiles(
-//   buffers: Buffer[],
-//   folder: string
-// ): Promise<{ publicId: string; url: string }[]> {
-//   const uploadPromises = buffers.map((buffer) =>
-//     new Promise<{ publicId: string; url: string }>((resolve, reject) => {
-//       const uploadStream = cloudinary.uploader.upload_stream(
-//         { folder, quality: "auto:eco" }, // Quality set to 'auto:eco' for better size optimization
-//         (error, result) => {
-//           if (error) {
-//             reject(new Error("Failed to upload file to Cloudinary."));
-//           } else {
-//             const publicId = result?.public_id || "";
-//             const url = result?.secure_url || "";
-//             resolve({ publicId, url });
-//           }
-//         }
-//       );
-//       uploadStream.end(buffer);
-//     })
-//   );
-
-//   return Promise.all(uploadPromises);
-// }
